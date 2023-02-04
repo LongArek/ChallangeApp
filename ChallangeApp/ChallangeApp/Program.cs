9a77019bece6388612b6b801a16d8deeb2059dc8
Arkadiusz Aleksiejuk <siejuk.ab@gmail.com>
2023-02-04 23:06:29 +0100

    Praca domowa dzien 6

---
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using ChallangeApp;
+
+var user1 = new Employee("Kazik", "Kowalski", "45 lat");
+var user2 = new Employee("Jolanta", "Malinowska", "23 lat");
+var user3 = new Employee("Edek", "Zkrainykredek", "58 lat");
+
+user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(10);
+user1.AddScore(2);
+user1.AddScore(10);
+
+user2.AddScore(8);
+user2.AddScore(9);
+user2.AddScore(10);
+user2.AddScore(2);
+user2.AddScore(10);
+
+user3.AddScore(10);
+user3.AddScore(10);
+user3.AddScore(10);
+user3.AddScore(2);
+user3.AddScore(10);
+
+List<Employee> users = new List<Employee>()
+{
+    user1, user2, user3
+};
+
+int maxResult = -1;
+Employee userWithMaxResult = null;
+
+foreach (var user in users)
+{
+    if (user.Result > maxResult)
+    {
+        userWithMaxResult = user;
+    }
+}
+
+Console.WriteLine("Pracownik, który otrzymał najwiekszą ilość punktów: " +
+    userWithMaxResult.Name+" "+userWithMaxResult.Surname+" "+userWithMaxResult.Age);
+Console.WriteLine("Punkty: "+ userWithMaxResult.Result);
+
+
+
+
